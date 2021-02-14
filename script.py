a5c91b10448fc750402aa23bfde4d44a538ba966
Raed Ahsan <79044784+Rsecurity1@users.noreply.github.com>
2021-02-14 22:06:49 +0300

    Update script.py

--- script.py
+++ script.py
@@ -4,6 +4,6 @@ pg.hotkey('winleft')
 
 pg.typewrite('chrome\n',0.5)
 
-pg.typewrite('www.linkedin.com/in/raed-ahsan/',0.2)
+pg.typewrite('',0.2) #In the  apostrophes , you can add the website url where you want to visit 
 
 pg.hotkey('winleft','up')
